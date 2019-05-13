f5c89f5cae2102a986ba1fbf83db2fa79684a6af
pedrocosta02 <henriquegamesrj3@gmail.com>
2019-05-13 15:46:21 -0300

    config

--- script.js
+++ script.js
@@ -13,16 +13,16 @@ audio10 = document.getElementById('audio10');
 			function play1(){
 				audio1.play();
 			}
-         function play2(){
-				audio2.play();
-         }
-         function play3(){
-				audio3.play();
-         }
-         function play4(){
-				audio4.play();
-         }
-         function play5(){
+			function play2(){
+		   		audio2.play();
+            }
+            function play3(){
+		   		audio3.play();
+            }
+            function play4(){
+		   		audio4.play();
+            }
+            function play5(){
 				audio5.play();
 			}
 			function play6(){
@@ -39,5 +39,37 @@ audio10 = document.getElementById('audio10');
 			}
 			function play10(){
 				audio10.play();
+			}
+			function stop(){
+				audio1.pause();
+				audio1.currentTime = 0;
+
+				audio2.pause();
+				audio2.currentTime = 0;
+
+				audio3.pause();
+				audio3.currentTime = 0;
+
+				audio4.pause();
+				audio4.currentTime = 0;
+
+				audio5.pause();
+				audio5.currentTime = 0;
+
+				audio6.pause();
+				audio6.currentTime = 0;
+
+				audio7.pause();
+				audio7.currentTime = 0;
+
+				audio8.pause();
+				audio8.currentTime = 0;
+
+				audio9.pause();
+				audio9.currentTime = 0;
+
+				audio10.pause();
+				audio10.currentTime = 0;
+
 			}
             
