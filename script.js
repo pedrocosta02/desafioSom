84a211d2a77b6656afac7384ef6ef7e5dbff40ee
pedrocosta02 <henriquegamesrj3@gmail.com>
2019-05-13 13:38:55 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+audio1 = document.getElementById('audio1');
+audio2 = document.getElementById('audio2');
+audio3 = document.getElementById('audio3');
+audio4 = document.getElementById('audio4');
+audio5 = document.getElementById('audio5');
+audio6 = document.getElementById('audio6');
+audio7 = document.getElementById('audio7');
+audio8 = document.getElementById('audio8');
+audio9 = document.getElementById('audio9');
+audio10 = document.getElementById('audio10');
+
+
+			function play1(){
+				audio1.play();
+			}
+         function play2(){
+				audio2.play();
+         }
+         function play3(){
+				audio3.play();
+         }
+         function play4(){
+				audio4.play();
+         }
+         function play5(){
+				audio5.play();
+			}
+			function play6(){
+				audio6.play();
+			}
+			function play7(){
+				audio7.play();
+			}
+			function play8(){
+				audio8.play();
+			}
+			function play9(){
+				audio9.play();
+			}
+			function play10(){
+				audio10.play();
+			}
+            
